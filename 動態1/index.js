0d9d0b209f38bf8242111ce894198154f2bd2444
toroy111 <134087247+toroy111@users.noreply.github.com>
2023-06-17 22:09:04 +0800

    version1

---
+++ 動態1/index.js
@@ -0,0 +1,7 @@
+(function(){
+	var nav = $('nav');
+	nav.delegate('a','click', function(e){
+	  nav.find('.active').removeClass('active');    
+	  $(e.target).closest('a').addClass('active');
+	})
+  })();
